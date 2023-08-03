9bc53825f2cc9ffc720aabac525ecbb5918d2c82
vssavin <savin1801@gmail.com>
2023-08-03 12:44:29 +0300

    Added rsa encryption/decryption using Java, not JavaScript because need a Nashorn compatible js library; Added jsencrypt.min.js - to use in browsers; Added tests for rsa encryption/decryption

---
+++ src/main/java/com/github/vssavin/jcrypt/js/JavaJsJCryptRSA.java
@@ -0,0 +1,128 @@
+package com.github.vssavin.jcrypt.js;
+
+import com.github.vssavin.jcrypt.StringSafety;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.security.*;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.stream.Collectors;
+
+/**
+ * @author vssavin on 03.08.2023
+ */
+public class JavaJsJCryptRSA extends JsJCryptEngine {
+    private static final String ENCRYPT_METHOD_NAME = "encodeRSA";
+    private static final String DECRYPT_METHOD_NAME = "decodeRSA";
+    private final Cipher rsaCipher;
+    private final KeyFactory keyFactory;
+
+    public JavaJsJCryptRSA(StringSafety stringSafety) {
+        super(stringSafety);
+        addPhantomScript("static/js/jsencrypt.min.js");
+        KeyFactory keyFactory1;
+        Cipher rsaCipher1;
+
+        try {
+            rsaCipher1 = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING");
+
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            rsaCipher1 = null;
+        }
+
+        rsaCipher = rsaCipher1;
+
+        try {
+            keyFactory1 = KeyFactory.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            keyFactory1 = null;
+        }
+
+        keyFactory = keyFactory1;
+    }
+
+    @Override
+    public String encrypt(String message, String key) {
+        if (key == null) {
+            return message;
+        }
+        key = normalizeKey(key);
+
+        Cipher encryptCipher;
+        String encrypted;
+        try {
+            encryptCipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING");
+            EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(key.getBytes()));
+            PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+            encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] encryptedBytes = encryptCipher.doFinal(message.getBytes());
+            byte[] base64 = Base64.getEncoder().encode(encryptedBytes);
+            Arrays.fill(encryptedBytes, (byte) 0);
+            encrypted = new String(base64);
+            Arrays.fill(base64, (byte) 0);
+
+        } catch (Exception e) {
+            throw new JsEncryptionException("Encryption error!", e);
+        }
+
+        return encrypted;
+    }
+
+    @Override
+    public String decrypt(String encrypted, String key) {
+        String decrypted;
+        try {
+            byte[] privateKeyBytes = Base64.getDecoder().decode(key.getBytes());
+            EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+            PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+            synchronized (rsaCipher) {
+                rsaCipher.init(Cipher.DECRYPT_MODE, privateKey);
+            }
+
+            byte[] base64 = Base64.getDecoder().decode(encrypted.getBytes());
+            byte[] decryptedBytes;
+            synchronized (rsaCipher) {
+                decryptedBytes = rsaCipher.doFinal(base64);
+            }
+
+            Arrays.fill(base64, (byte) 0);
+            decrypted = new String(decryptedBytes);
+            Arrays.fill(decryptedBytes, (byte) 0);
+        } catch (InvalidKeySpecException | InvalidKeyException |
+                BadPaddingException | IllegalBlockSizeException e) {
+            throw new JsDecryptionException("Decryption error!", e);
+        }
+
+        return decrypted;
+    }
+
+    @Override
+    public String getEncryptMethodName() {
+        return ENCRYPT_METHOD_NAME;
+    }
+
+    @Override
+    public String getDecryptMethodName() {
+        return DECRYPT_METHOD_NAME;
+    }
+
+    private String normalizeKey(String key) {
+        if (key != null) {
+            BufferedReader reader = new BufferedReader(new StringReader(key));
+            key = reader.lines().filter(s ->
+                            !s.trim().toLowerCase().startsWith("--") && !s.trim().toLowerCase().startsWith("comment"))
+                    .collect(Collectors.joining());
+        }
+
+        return key;
+    }
+}
--- src/test/java/com/github/vssavin/jcrypt/JCryptTest.java
+++ src/test/java/com/github/vssavin/jcrypt/JCryptTest.java
@@ -1,19 +1,25 @@
 package com.github.vssavin.jcrypt;
 
+import com.github.vssavin.jcrypt.js.JavaJsJCryptRSA;
 import com.github.vssavin.jcrypt.js.JsJCryptAES;
 import com.github.vssavin.jcrypt.js.JsJCryptStub;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.UUID;
 
 /**
  * @author vssavin on 02.08.2023
  */
 public class JCryptTest {
+
     @Test
-    public void aesEncryptDecryptTest(){
+    public void aesEncryptDecryptTest() {
         JCrypt aes = new JsJCryptAES(str -> {
         });
 
@@ -28,7 +34,29 @@ public class JCryptTest {
     }
 
     @Test
-    public void stubEncryptDecryptTest(){
+    public void rsaEncryptDecryptTest() throws NoSuchAlgorithmException {
+        JCrypt rsa = new JavaJsJCryptRSA(str -> {
+        });
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair pair = keyPairGenerator.generateKeyPair();
+        byte[] publicKeyBytes = pair.getPublic().getEncoded();
+        byte[] privateKeyBytes = pair.getPrivate().getEncoded();
+
+        String sourceMessage = "Test message";
+        String key = Base64.getEncoder().encodeToString(publicKeyBytes);
+        String encrypted = rsa.encrypt(sourceMessage, key);
+        Assert.assertNotEquals(encrypted, sourceMessage);
+
+        key = Base64.getEncoder().encodeToString(privateKeyBytes);
+        String decrypted = rsa.decrypt(encrypted, key);
+        Assert.assertNotEquals(encrypted, decrypted);
+        Assert.assertEquals(sourceMessage, decrypted);
+    }
+
+    @Test
+    public void stubEncryptDecryptTest() {
         JCrypt stub = new JsJCryptStub(str -> {
         });
 
