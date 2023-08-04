6a42dac48e1c3286d5ed5c7c937d898da1d00823
vssavin <savin1801@gmail.com>
2023-08-04 11:38:49 +0300

    added tests for DefaultPlatformStorage, LinuxPlatformStorage, WindowsPlatformStorage

---
+++ src/test/java/com/github/vssavin/jcrypt/osplatform/OSPlatformCryptTest.java
@@ -0,0 +1,62 @@
+package com.github.vssavin.jcrypt.osplatform;
+
+import com.github.vssavin.jcrypt.JCrypt;
+import com.github.vssavin.jcrypt.JKeyStorage;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OSPlatformCryptTest {
+
+    @Test
+    public void defaultPlatformStorageTest() {
+        String message = "test message";
+        JKeyStorage platformStorage = new DefaultPlatformStorage();
+        String key = platformStorage.getKey();
+        JCrypt storageSecure = new OSPlatformCrypt();
+        String encrypted = storageSecure.encrypt(message, key);
+        String decrypted = storageSecure.decrypt(encrypted, key);
+
+        Assert.assertEquals(message, decrypted);
+    }
+
+    @Test
+    public void windowsPlatformStorageTest() {
+        String message = "test message";
+        JKeyStorage platformStorage;
+        String key;
+        try {
+            platformStorage = new WindowsPlatformStorage();
+            key = platformStorage.getKey();
+        } catch (PlatformInitException e) {
+            platformStorage = new DefaultPlatformStorage();
+            key = platformStorage.getKey();
+        }
+
+        JCrypt storageSecure = new OSPlatformCrypt();
+        String encrypted = storageSecure.encrypt(message, key);
+        String decrypted = storageSecure.decrypt(encrypted, key);
+
+        Assert.assertEquals(message, decrypted);
+    }
+
+    @Test
+    public void linuxPlatformStorageTest() {
+        String message = "test message";
+        JKeyStorage platformStorage;
+        String key;
+        try {
+            platformStorage = new LinuxPlatformStorage();
+            key = platformStorage.getKey();
+        } catch (PlatformInitException e) {
+            platformStorage = new DefaultPlatformStorage();
+            key = platformStorage.getKey();
+        }
+
+        JCrypt storageSecure = new OSPlatformCrypt();
+        String encrypted = storageSecure.encrypt(message, key);
+        String decrypted = storageSecure.decrypt(encrypted, key);
+
+        Assert.assertEquals(message, decrypted);
+    }
+
+}
